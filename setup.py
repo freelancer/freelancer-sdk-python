216b1ce0f5ed0cdf349eaf44d29aa44e00aabc83
Amit Saha <asaha@freelancer.com>
2017-12-18 14:00:41 +1100

    Update trove classifier

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     description='Freelancer.com SDK',
     long_description='Freelancer.com SDK',
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Topic :: Software Development",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         'Operating System :: OS Independent',
