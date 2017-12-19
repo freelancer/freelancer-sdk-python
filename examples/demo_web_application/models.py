71ea73e6f2acdcd7ed7d9b054eea0722597be337
Christopher Chaaya <cchaaya@freelancer.com>
2017-12-19 15:20:43 +1100

    Fix setup files so that README flow works correctly. Add venv to .gitignore. Add naive project storage to DB. Fix requirements.txt error when installing via pip.

--- examples/demo_web_application/models.py
+++ examples/demo_web_application/models.py
@@ -1,7 +1,10 @@
+from flask import Flask
 from sqlalchemy.dialects.postgresql import JSON
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -11,9 +14,20 @@ class User(db.Model):
     email = db.Column(db.String())
     access_token = db.Column(db.String())
     refresh_token = db.Column(db.String())
-
+    projects = db.relationship("Project", backref="users", uselist=False)
     def __init__(self, name, email, access_token, refresh_token):
         self.name = name
         self.email = email
         self.access_token = access_token
         self.refresh_token = refresh_token
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.String())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __init__(self, project_id, user_id):
+        self.project_id = project_id
+        self.user_id = user_id
