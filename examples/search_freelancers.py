a12731a46bb5bb2f205d184c5463f3808ab2d6a3
gnavarro <gnavarro@freelancer.com>
2018-03-27 15:30:38 +0800

    add user getters

---
+++ examples/search_freelancers.py
@@ -0,0 +1,36 @@
+from freelancersdk.session import Session
+from freelancersdk.resources.users.users import search_freelancers
+from freelancersdk.resources.users.exceptions import \
+    UsersNotFoundException
+from freelancersdk.resources.users.helpers import (
+    create_get_users_details_object
+)
+
+import os
+
+
+def sample_search_freelancers():
+    url = os.environ.get('FLN_URL')
+    oauth_token = os.environ.get('FLN_OAUTH_TOKEN')
+    session = Session(oauth_token=oauth_token, url=url)
+    user_details = create_get_users_details_object(
+        country=True,
+        profile_description=True
+    )
+    try:
+        result = search_freelancers(
+            session,
+            username='CodeJunk',
+            user_details=user_details
+        )
+    except UsersNotFoundException as e:
+        print('Error message: {}'.format(e.message))
+        print('Server response: {}'.format(e.error_code))
+        return None
+    else:
+        return result
+
+result = sample_search_freelancers()
+
+if result:
+    print('Found freelancers: {}'.format(len(result)))
